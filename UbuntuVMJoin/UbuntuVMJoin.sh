22de9f1d70b5367a94bde08a147dbd60aa0bcd53
miltonb <59422970+miltonms@users.noreply.github.com>
2020-05-24 12:48:36 -0600

    Update UbuntuVMJoin.sh

--- UbuntuVMJoin/UbuntuVMJoin.sh
+++ UbuntuVMJoin/UbuntuVMJoin.sh
@@ -5,101 +5,101 @@
 
 #Get the domain name in a variable
 read -p "Please enter the managed instance domain name (Example: example.com): " domainName
-sudo echo ""
+echo ""
 
 #Modify /etc/hosts file with 127.0.0.1 ubuntu.aaddscontoso.com ubuntu
-sudo echo "Modifing the /etc/hosts file"
+echo "Modifing the /etc/hosts file"
 sudo sed -i -r "/^127.0.0.1 localhost/i 127.0.0.1 $( echo $(hostname).$domainName $(hostname) | tr '[:upper:]' '[:lower:]')" /etc/hosts
-sudo echo "grep output from /etc/hosts file"
+echo "grep output from /etc/hosts file"
 sudo cat /etc/hosts | grep 127.0.0.1
-sudo echo ""
+echo ""
 
 #Install required components
-sudo echo "Installing required packages krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli"
+echo "Installing required packages krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli"
 sudo apt-get update
 sudo apt-get --assume-yes install krb5-user samba sssd sssd-tools libnss-sss libpam-sss ntp ntpdate realmd adcli
-sudo echo ""
+echo ""
 
 #Modify the /etc/ntp.conf file with server aaddscontoso.com
-sudo echo "Modifing the /etc/ntp.conf file"
+echo "Modifing the /etc/ntp.conf file"
 sudo sed -i -r "1 i server $domainName" /etc/ntp.conf
-sudo echo "grep output from /etc/ntp.conf file"
+echo "grep output from /etc/ntp.conf file"
 sudo cat /etc/ntp.conf | grep server
-sudo echo ""
+echo ""
 
 #Stop, update and start the ntp service
-sudo echo "Stopping, updating and starting the ntp service"
+echo "Stopping, updating and starting the ntp service"
 sudo systemctl stop ntp
 sudo ntpdate $domainName
 sudo systemctl start ntp
-sudo echo ""
+echo ""
 
 #discover the realm
-sudo echo "Discovering the realm"
+echo "Discovering the realm"
 sudo realm discover ${domainName^^}
-sudo echo ""
+echo ""
 
 #Get the admin user who will join the VM to the managed instance
 read -p "Please enter the domain admin name (Example: admin): " domainAdmin
-sudo echo ""
+echo ""
 
 #Initialize the kinit process
-sudo echo "Starting the kinit process"
+echo "Starting the kinit process"
 kinit $domainAdmin@${domainName^^}
-sudo echo ""
+echo ""
 
 #Join the VM
-sudo echo "Joining the VM to the AAD DS managed instance"
+echo "Joining the VM to the AAD DS managed instance"
 sudo realm join --verbose ${domainName^^} -U "$domainAdmin@${domainName^^}" --install=/
-sudo echo ""
+echo ""
 
 #Modify the /etc/krb5.conf file with rdns=false
-sudo echo "Modifing the /etc/krb5.conf file"
+echo "Modifing the /etc/krb5.conf file"
 sudo sed -i -r "/default_realm/a \\\trdns=false" /etc/krb5.conf
-sudo echo "grep output from /etc/krb5.conf file"
+echo "grep output from /etc/krb5.conf file"
 sudo cat /etc/krb5.conf | grep rdns
-sudo echo ""
+echo ""
 
 #Modify the /etc/sssd/sssd.conf file with # use_fully_qualified_names = True
-sudo echo "Modifing the /etc/sssd/sssd.conf file"
+echo "Modifing the /etc/sssd/sssd.conf file"
 sudo sed -i -r 's/use_fully_qualified_names = True/#use_fully_qualified_names = True/' /etc/sssd/sssd.conf
-sudo echo "grep output from /etc/sssd/sssd.conf file"
+echo "grep output from /etc/sssd/sssd.conf file"
 sudo cat /etc/sssd/sssd.conf | grep use_fully_qualified_names
-sudo echo ""
+echo ""
 
 #Restart the sssd service
-sudo echo "Restarting the sssd service"
+echo "Restarting the sssd service"
 sudo service sssd restart
-sudo echo ""
+echo ""
 
 #Modify the /etc/ssh/sshd_config file with PasswordAuthentication yes
-sudo echo "Modifing the /etc/ssh/sshd_config file"
+echo "Modifing the /etc/ssh/sshd_config file"
 sudo sed -i -r 's/^(PasswordAuthentication (n|N)o|#PasswordAuthentication (n|N)o|#PasswordAuthentication yes)/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo echo "grep output from /etc/ssh/sshd_config file"
+echo "grep output from /etc/ssh/sshd_config file"
 sudo cat /etc/ssh/sshd_config | grep 'PasswordAuthentication yes'
-sudo echo ""
+echo ""
 
 #Restart the ssh service
-sudo echo "Restarting the ssh service"
+echo "Restarting the ssh service"
 sudo systemctl restart ssh
-sudo echo ""
+echo ""
 
 #Modify the /etc/pam.d/common-session file with session required pam_mkhomedir.so skel=/etc/skel/ umask=0077
-sudo echo "Modifing the /etc/pam.d/common-session file"
+echo "Modifing the /etc/pam.d/common-session file"
 sudo sed -i -r '/pam_sss.so/a session required pam_mkhomedir.so skel=/etc/skel/ umask=0077' /etc/pam.d/common-session
-sudo echo "grep output from /etc/pam.d/common-session file"
+echo "grep output from /etc/pam.d/common-session file"
 sudo cat /etc/pam.d/common-session | grep 'session required pam_mkhomedir.so skel=/etc/skel/ umask=0077'
-sudo echo ""
+echo ""
 
 #Modify /etc/sudoers file with "# Add 'AAD DC Administrators' group members as admins." & "%AAD\ DC\ Administrators ALL=(ALL) NOPASSWD:ALL"
-sudo echo "Modifing the /etc/sudoers file"
+echo "Modifing the /etc/sudoers file"
 echo "# Add 'AAD DC Administrators' group members as admins." | sudo tee -a /etc/sudoers
 echo "%AAD\ DC\ Administrators ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
-sudo echo "grep output from /etc/sudoers file"
+echo "grep output from /etc/sudoers file"
 sudo cat /etc/sudoers | grep 'AAD[\] DC[\] Administrators'
-sudo echo ""
+echo ""
 
 #Sign in with the domain admin user
-sudo echo "Signing with the domain admin user"
+echo "Signing with the domain admin user"
 ssh -l $domainAdmin@$domainName $(hostname).$domainName
-sudo echo ""
+echo ""
