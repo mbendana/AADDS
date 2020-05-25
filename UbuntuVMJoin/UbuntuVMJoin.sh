a9de09661cf0a0387f8c6369d2a62c996dff0eb5
miltonb <59422970+miltonms@users.noreply.github.com>
2020-05-24 19:11:01 -0600

    Update UbuntuVMJoin.sh

--- UbuntuVMJoin/UbuntuVMJoin.sh
+++ UbuntuVMJoin/UbuntuVMJoin.sh
@@ -9,7 +9,7 @@ read -p "Please enter the managed instance domain name (Example: aaddscontoso.co
 echo ""
 
 #Modify /etc/hosts file with 127.0.0.1 ubuntu.aaddscontoso.com ubuntu
-echo "Modifing the /etc/hosts file"
+echo "Modifying the /etc/hosts file"
 sudo sed -i -r "/^127.0.0.1/i 127.0.0.1 $( echo $(hostname).$domainName $(hostname) | tr '[:upper:]' '[:lower:]')" /etc/hosts
 echo "grep output from /etc/hosts file"
 sudo cat /etc/hosts | grep 127.0.0.1
@@ -22,7 +22,7 @@ sudo apt-get --assume-yes install krb5-user samba sssd sssd-tools libnss-sss lib
 echo ""
 
 #Modify the /etc/ntp.conf file with server aaddscontoso.com
-echo "Modifing the /etc/ntp.conf file"
+echo "Modifying the /etc/ntp.conf file"
 sudo sed -i -r "1 i server $domainName" /etc/ntp.conf
 echo "grep output from /etc/ntp.conf file"
 sudo cat /etc/ntp.conf | grep server
@@ -55,14 +55,14 @@ sudo realm join --verbose ${domainName^^} -U "$domainAdmin@${domainName^^}" --in
 echo ""
 
 #Modify the /etc/krb5.conf file with rdns=false
-echo "Modifing the /etc/krb5.conf file"
+echo "Modifying the /etc/krb5.conf file"
 sudo sed -i -r "/default_realm/a \\\trdns=false" /etc/krb5.conf
 echo "grep output from /etc/krb5.conf file"
 sudo cat /etc/krb5.conf | grep rdns
 echo ""
 
 #Modify the /etc/sssd/sssd.conf file with # use_fully_qualified_names = True
-echo "Modifing the /etc/sssd/sssd.conf file"
+echo "Modifying the /etc/sssd/sssd.conf file"
 sudo sed -i -r 's/use_fully_qualified_names = True/#use_fully_qualified_names = True/' /etc/sssd/sssd.conf
 echo "grep output from /etc/sssd/sssd.conf file"
 sudo cat /etc/sssd/sssd.conf | grep use_fully_qualified_names
@@ -74,7 +74,7 @@ sudo service sssd restart
 echo ""
 
 #Modify the /etc/ssh/sshd_config file with PasswordAuthentication yes
-echo "Modifing the /etc/ssh/sshd_config file"
+echo "Modifying the /etc/ssh/sshd_config file"
 sudo sed -i -r 's/^(PasswordAuthentication (n|N)o|#PasswordAuthentication (n|N)o|#PasswordAuthentication yes)/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo "grep output from /etc/ssh/sshd_config file"
 sudo cat /etc/ssh/sshd_config | grep 'PasswordAuthentication yes'
@@ -86,14 +86,14 @@ sudo systemctl restart ssh
 echo ""
 
 #Modify the /etc/pam.d/common-session file with session required pam_mkhomedir.so skel=/etc/skel/ umask=0077
-echo "Modifing the /etc/pam.d/common-session file"
+echo "Modifying the /etc/pam.d/common-session file"
 sudo sed -i -r '/pam_sss.so/a session required pam_mkhomedir.so skel=/etc/skel/ umask=0077' /etc/pam.d/common-session
 echo "grep output from /etc/pam.d/common-session file"
 sudo cat /etc/pam.d/common-session | grep 'session required pam_mkhomedir.so skel=/etc/skel/ umask=0077'
 echo ""
 
 #Modify /etc/sudoers file with "# Add 'AAD DC Administrators' group members as admins." & "%AAD\ DC\ Administrators ALL=(ALL) NOPASSWD:ALL"
-echo "Modifing the /etc/sudoers file"
+echo "Modifying the /etc/sudoers file"
 echo "# Add 'AAD DC Administrators' group members as admins." | sudo tee -a /etc/sudoers
 echo "%AAD\ DC\ Administrators ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 echo "grep output from /etc/sudoers file"
